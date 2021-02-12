b77875bfba4b9254c9e853fa7040dfb5de0fbc3c
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2021-02-12 11:49:47 -0500

    Downloading Percipitation Data

--- miner.py
+++ miner.py
@@ -2,19 +2,45 @@
 # All Right Reserved
 
 import requests
+import shutil
 
-year = "2020" # year of december
-headLink = "https://www.ncei.noaa.gov/data/national-digital-guidance-database/access/historical/"
+headLink = "https://www.ncei.noaa.gov/data/national-digital-guidance-database/access/"
 
-for year in ("2020",):
+# Parse Through Date Components
+for winterYear in ("2018",):
 	for month in ("12", "01", "02", "03"):
+		#Move year forward if after december
+		year = winterYear
+		if month != "12":
+			year = str(int(winterYear) + 1)
+
+		# Is data in historical folder
+		if year + month in ("202102", "202101", "202012", "202011", "202010", "202009", "202008", "202007", "202006"): #"202005", "201912", "201911", "201910", "201909", "201908", "201907", "201906", "201905", "201904", "201903", "201902", "201812", "201811", "201711"]:
+			historical = ""
+		else:
+			historical = "historical/"
 		for day in ("01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"):
-			if int(day) > 29 or (int(day) > 28 and int(year) % 4 != 0):
+			# Skip if extra days of febuary
+			if month == "02" and (int(day) > 29 or (int(day) > 28 and int(year) % 4 != 0)):
 				continue
-			for hour in ("0500", "0600", "0700"):
+
+			# May's Data is split in the middle
+			if year + month == "202005" and int(day) <= 15:
+				historical = "historical/"
+			elif year + month == "202005":
+				historical = ""
+
+			for hour in ("0500",):
+				# Create File Link
 				fileName = "LEIA98_KWBR_" + year + month + day + hour
-				link = headLink + year + month + "/" + year + month + day + "/" + fileName
-				print(link)
+				link = headLink + historical + year + month + "/" + year + month + day + "/" + fileName
+
+				# Save File
+				r = requests.get(link, stream=True)
+				r.raw.decode_content = True
+				with open("data/" + fileName, 'wb') as f:
+					shutil.copyfileobj(r.raw, f)
+					print("Data Saved: " + link)
 
 
 
