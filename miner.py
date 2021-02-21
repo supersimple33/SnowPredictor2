0d3e217adb4016b3d762d82c61b879129225b610
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2021-02-21 15:57:10 -0500

    Working Data Miner
    - Extracted some variables
    
    - Plugged in correct dates and times
    
    - Added better error handling and exit

--- miner.py
+++ miner.py
@@ -5,14 +5,17 @@ import requests
 import shutil
 
 headLink = "https://www.ncei.noaa.gov/data/national-digital-guidance-database/access/"
+header = "LEIA98_KWBR_"
+missed = []
 
 # Parse Through Date Components
-for winterYear in ("2018",):
+for winterYear in ("2019",):
 	for month in ("12", "01", "02", "03"):
 		#Move year forward if after december
 		year = winterYear
 		if month != "12":
 			year = str(int(winterYear) + 1)
+		print("Started: " + year + "/" + month)
 
 		# Is data in historical folder
 		if year + month in ("202102", "202101", "202012", "202011", "202010", "202009", "202008", "202007", "202006"): #"202005", "201912", "201911", "201910", "201909", "201908", "201907", "201906", "201905", "201904", "201903", "201902", "201812", "201811", "201711"]:
@@ -30,14 +33,34 @@ for winterYear in ("2018",):
 			elif year + month == "202005":
 				historical = ""
 
-			for hour in ("0500",):
+			for hour in ("0000", "0100", "0200", "0300", "0400", "0500", "0600", "0700", "0800", "0900", "1000", "1100", "1200", "1300", "1400", "1500", "1600", "1700", "1800", "1900", "2000", "2100", "2200", "2300"):
 				# Create File Link
-				fileName = "LTIA98_KWBR_" + year + month + day + hour
+				fileName = header + year + month + day + hour
 				link = headLink + historical + year + month + "/" + year + month + day + "/" + fileName
 
-				# Save File
+				# Request Data
 				r = requests.get(link, stream=True)
-				r.raw.decode_content = True
-				with open("data/" + fileName, 'wb') as f:
-					shutil.copyfileobj(r.raw, f)
-					print("Data Saved: " + link)
+				if r.status_code == 200:
+					r.raw.decode_content = True
+					with open("data/" + fileName, 'wb') as f:
+						shutil.copyfileobj(r.raw, f)
+				else:
+					# If fail search alternate directory and then check
+					hisTemp = "historical/" if historical == "" else ""
+					link = headLink + hisTemp + year + month + "/" + year + month + day + "/" + fileName
+
+					# Request Data Again
+					r = requests.get(link, stream=True)
+					if r.status_code == 200:
+						r.raw.decode_content = True
+						with open("data/" + fileName, 'wb') as f:
+							shutil.copyfileobj(r.raw, f)
+						print("Retreived " + hisTemp + fileName + " on alternate")
+					else:
+						print("Failed to get " + fileName + ", reason: " + r.reason + ", code: " + str(r.status_code))
+						missed.append(fileName)
+
+# Note missed files so they may be recovered through other methods
+with open("data/missed.txt", 'w') as fp:
+	fp.write('\n'.join(missed) + '\n')
+print("Finished with " + str(len(missed)) + " files skipped")
