c055abe3f4eba294c41a51d87f9908ba885d2334
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2021-02-21 14:37:35 -0500

    Polished Weather Extractor
    - Renamed temps to weather
    
    - Switched from os.listdir to pathlib.Path for better recursive searching
    
    - Added comments
    
    - Added progress tracker

--- weatherExtractor.py
+++ weatherExtractor.py
@@ -1,40 +1,55 @@
-from os import listdir
+from pathlib import Path
 from os.path import isfile, join
 import pygrib
 import numpy as np
 import json
 
-temps = {}
+weather = {}
 
 dayComp = ""
 
 def populate(y, m, d):
-	if y in temps:
-		if m in temps[y]:
-			if d not in temps[y][m]:
-				temps[y][m][d] = {}
+	if y in weather:
+		if m in weather[y]:
+			if d not in weather[y][m]:
+				weather[y][m][d] = {}
 		else:
-			temps[y][m] = {}
-			temps[y][m][d] = {}
+			weather[y][m] = {}
+			weather[y][m][d] = {}
 	else:
-		temps[y] = {}
-		temps[y][m] = {}
-		temps[y][m][d] = {}
+		weather[y] = {}
+		weather[y][m] = {}
+		weather[y][m][d] = {}
 
-for f in listdir("temperatures"):
-	grbs = pygrib.open("temperatures/" + f)
+
+# Get a list and iterate through all gribs
+files = list(Path("./data").rglob("LEIA*"))
+i = 0
+for loc in files:
+	f = str(loc)
+	grbs = pygrib.open(str(f))
+	f = f[5:]
 	for elem in grbs:
-		if elem['average'] > 50:
-			grb = elem
+		# # Temperature check only
+		# if elem['average'] > 25:
+		# 	grb = elem
+		grb = elem
 	dat = grb.data(lat1=41.11, lat2=41.18, lon1=-73.55, lon2=-73.45)
+
+	grbs.close()
 
 	y = int(f[12:16])
 	m = int(f[16:18])
 	d = int(f[18:20])
 	t = int(f[20:24])
-	populate(y,m,d)
-	temps[y][m][d][t] = sum(dat[0]) / (len(dat[0])) #take median to avoid skewing
-	print(f)
 
-with open('data.json', 'w') as fp:
-	json.dump(temps, fp, indent=2, sort_keys=True)
+	populate(y,m,d)
+	weather[y][m][d][t] = sum(dat[0]) / (len(dat[0])) #could take median to avoid skewing
+	
+	i += 1
+	if i % (len(files) // 20) == 0:
+		print(i / len(files))
+
+# Save data in JSON
+with open('data2019p.json', 'w') as fp:
+	json.dump(weather, fp, indent=2, sort_keys=True)
