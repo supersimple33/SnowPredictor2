8ec58c20a8435c1a49ac5db84935863802f40d14
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2021-02-25 19:05:33 -0500

    Edited And Ran Weather Extracor

--- weatherExtractor.py
+++ weatherExtractor.py
@@ -23,25 +23,32 @@ def populate(y, m, d):
 
 
 # Get a list and iterate through all gribs
-files = list(Path("./data").rglob("LEIA*"))
+files = list(Path("./master_data/temp2").rglob("YGB*"))
 i = 0
+offset = 12
 for loc in files:
 	f = str(loc)
 	grbs = pygrib.open(str(f))
-	f = f[5:]
+	f = f.split('/')[-1]
+
+	# Extract Dates
+	y = int(f[offset : offset+4])
+	m = int(f[offset+4 : offset+6])
+	d = int(f[offset+6 : offset+8])
+	t = int(f[offset+8 : offset+12])
+	datNum = (y * 10000) + (m * 100) + d
+
+	fct = 10000
 	for elem in grbs:
 		# # Temperature check only
 		# if elem['average'] > 25:
 		# 	grb = elem
-		grb = elem
+		if elem['forecastTime'] < fct:
+			grb = elem
+			fct = elem['forecastTime']
 	dat = grb.data(lat1=41.11, lat2=41.18, lon1=-73.55, lon2=-73.45)
 
 	grbs.close()
-
-	y = int(f[12:16])
-	m = int(f[16:18])
-	d = int(f[18:20])
-	t = int(f[20:24])
 
 	populate(y,m,d)
 	weather[y][m][d][t] = sum(dat[0]) / (len(dat[0])) #could take median to avoid skewing
@@ -51,5 +58,5 @@ for loc in files:
 		print(i / len(files))
 
 # Save data in JSON
-with open('data2019p.json', 'w') as fp:
+with open('data2019maxt.json', 'w') as fp:
 	json.dump(weather, fp, indent=2, sort_keys=True)
