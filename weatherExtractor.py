5bbcfcb3902ebc66c887bb1966ad91d6fad68324
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2021-03-11 15:36:31 -0500

    Weather extraction uses future's multithreading

--- weatherExtractor.py
+++ weatherExtractor.py
@@ -1,8 +1,7 @@
 from pathlib import Path
-from os.path import isfile, join
 import pygrib
-import numpy as np
 import json
+from concurrent import futures
 
 weather = {}
 
@@ -21,41 +20,42 @@ def populate(y, m, d):
 		weather[y][m] = {}
 		weather[y][m][d] = {}
 
+def extract(file):
+	grbs = pygrib.open(file)
 
-# Get a list and iterate through all gribs
-files = list(Path("./master_data/temp2").rglob("YGB*"))
-i = 0
-offset = 12
-for loc in files:
-	f = str(loc)
-	grbs = pygrib.open(str(f))
-	f = f.split('/')[-1]
-
-	# Extract Dates
-	y = int(f[offset : offset+4])
-	m = int(f[offset+4 : offset+6])
-	d = int(f[offset+6 : offset+8])
-	t = int(f[offset+8 : offset+12])
-	datNum = (y * 10000) + (m * 100) + d
-
-	fct = 10000
-	for elem in grbs:
-		# # Temperature check only
-		# if elem['average'] > 25:
-		# 	grb = elem
-		if elem['forecastTime'] < fct:
-			grb = elem
-			fct = elem['forecastTime']
-	dat = grb.data(lat1=41.11, lat2=41.18, lon1=-73.55, lon2=-73.45)
+	measurements = []
+	for grb in grbs:
+		dat = grb.data(lat1=41.11, lat2=41.18, lon1=-73.55, lon2=-73.45)
+		measurements.append(sum(dat[0]) / (len(dat[0]))) #could take median to avoid skewing
 
 	grbs.close()
+	return measurements
 
-	populate(y,m,d)
-	weather[y][m][d][t] = sum(dat[0]) / (len(dat[0])) #could take median to avoid skewing
-	
-	i += 1
-	if i % (len(files) // 20) == 0:
-		print(i / len(files))
+# Get a list and iterate through all gribs
+files = list(Path("./data").rglob("LEIA*"))
+i = 0
+offset = 12
+
+with futures.ThreadPoolExecutor() as executor:
+	future_extract = dict((executor.submit(extract, str(loc)), str(loc)) for loc in files)
+
+	i = 0
+	for future in futures.as_completed(future_extract):
+		file = future_extract[future]
+		if future.exception() is not None:
+			print('%r generated an exception: %s' % (file, future.exception()))
+		else:
+			f = file.split('/')[-1]
+			# Extract Dates
+			y = int(f[offset : offset+4])
+			m = int(f[offset+4 : offset+6])
+			d = int(f[offset+6 : offset+8])
+			t = int(f[offset+8 : offset+12])
+			populate(y,m,d)
+			weather[y][m][d][t] = future.result()[0]
+		if i % (len(files) // 20) == 0:
+			print(i / len(files))
+		i += 1
 
 # Save data in JSON
 with open('data2019maxt.json', 'w') as fp:
