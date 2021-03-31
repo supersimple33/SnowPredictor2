d86bfaceb6edfc4a004262b515bf279cea001e4f
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2021-03-30 20:24:50 -0400

    Collected and sorted data

--- singleDataStruct.py
+++ singleDataStruct.py
@@ -2,19 +2,19 @@ import json
 import pandas as pd
 
 # structure = {'date', 'minTemp', 'maxTemp', 'percipitation', 'wind'}
-structure = ['date', 'temperature', 'percipitation', 'wind', 'windGust']
+structure = ['date', 'minTemp', 'maxTemp', 'percipitation', 'wind', 'windGust']
 rows = []
 
-with open("rt2019wind3.json", 'r') as f:
+with open("rt2017temp.json", 'r') as f:
 	temp = json.load(f)
 
-with open("rt2019p.json", 'r') as f:
+with open("rt2017p.json", 'r') as f:
 	percip = json.load(f)
 
-with open("rt2019wind2.json", 'r') as f:
+with open("rt2017wind.json", 'r') as f:
 	wind = json.load(f)
 
-with open("rt2019wind1.json", 'r') as f:
+with open("rt2017gust.json", 'r') as f:
 	gust = json.load(f)
 
 for y in temp.keys():
@@ -25,9 +25,9 @@ for y in temp.keys():
 				print('A data group was missing data on ' + m + '/' + d + '/' + y)
 			
 			# Get temperature data
-			avgTemp = sum(temp[y][m][d].values()) / len(temp[y][m][d])
-			# maxTemp = max(temp[y][m][d].values())
-			# minTemp = min(temp[y][m][d].values())
+			# avgTemp = sum(temp[y][m][d].values()) / len(temp[y][m][d])
+			maxTemp = max(temp[y][m][d].values())
+			minTemp = min(temp[y][m][d].values())
 
 			# Get percip data
 			totalPercipitation = sum(percip[y][m][d].values())
@@ -38,9 +38,9 @@ for y in temp.keys():
 			# Get gust data
 			maxGust = max(gust[y][m][d].values())
 
-			elem = [m + '/' + d + '/' + y, avgTemp, totalPercipitation, avgWind, maxGust]
+			elem = [m + '/' + d + '/' + y, minTemp, maxTemp, totalPercipitation, avgWind, maxGust]
 			rows.append(elem)
 
 df = pd.DataFrame(data=rows, columns=structure)
 
-df.to_csv("output.csv")
+df.to_csv("output17.csv")
